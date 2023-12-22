9bf1296c099a7c498293829a7fd3915416350810
Lau <hi@lau.work>
2023-12-21 21:26:43 -0300

    refactor: add customizable "opening" and "closing" states

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Service, Characteristic, HomebridgeAPI;
 
-module.exports = function(homebridge) {
+module.exports = function (homebridge) {
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
 	HomebridgeAPI = homebridge;
@@ -8,16 +8,18 @@ module.exports = function(homebridge) {
 }
 
 class DummyGarage {
-	constructor (log, config) {
+	constructor(log, config) {
 
 		//get config values
 		this.name = config['name'] || "Dummy Garage";
-		this.autoCloseDelay = config["autoCloseDelay"] === undefined ? 0 : Number(config["autoCloseDelay"]);
-		
+		this.openingStateDuration = config["openingStateDuration"] === undefined ? 0 : Number(config["openingStateDuration"]);
+		this.openStateDuration = config["openStateDuration"] === undefined ? 0 : Number(config["openStateDuration"]);
+		this.closingStateDuration = config["closingStateDuration"] === undefined ? 0 : Number(config["closingStateDuration"]);
+
 		//persist storage
 		this.cacheDirectory = HomebridgeAPI.user.persistPath();
 		this.storage = require('node-persist');
-		this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
+		this.storage.initSync({ dir: this.cacheDirectory, forgiveParseErrors: true });
 		this.cachedState = this.storage.getItemSync(this.name);
 
 		//initial setup
@@ -25,64 +27,72 @@ class DummyGarage {
 		this.lastOpened = new Date();
 		this.service = new Service.GarageDoorOpener(this.name, this.name);
 		this.setupGarageDoorOpenerService(this.service);
-		
+
 		this.informationService = new Service.AccessoryInformation();
 		this.informationService
 			.setCharacteristic(Characteristic.Manufacturer, 'github/rasod')
 			.setCharacteristic(Characteristic.Model, 'Dummy Garage')
 			.setCharacteristic(Characteristic.FirmwareRevision, '1.2.2')
 			.setCharacteristic(Characteristic.SerialNumber, this.name.replace(/\s/g, '').toUpperCase());
-}
-
-getServices () {
-	return [this.informationService, this.service];
-}
-
-setupGarageDoorOpenerService (service) {
-	this.log.debug("setupGarageDoorOpenerService");
-	this.log.debug("Cached State: " + this.cachedState);
-	
-	if((this.cachedState === undefined) || (this.cachedState === true)) {
-		this.log.debug("Using Saved OPEN State");
-		this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN);
-	} else {
-		this.log.debug("Using Default CLOSED State");
-		this.service.setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.CLOSED);
-		this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED);
 	}
 
-	service.getCharacteristic(Characteristic.TargetDoorState)
-		.on('get', (callback) => {
-			var targetDoorState = service.getCharacteristic(Characteristic.TargetDoorState).value;
-			callback(null, targetDoorState);
-		})
-		.on('set', (value, callback) => {
-			if (value === Characteristic.TargetDoorState.OPEN) {
-				this.log("Opening: " + this.name)
-				this.lastOpened = new Date();
-				this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN);
-				this.storage.setItem(this.name, true);
-				this.log.debug("autoCloseDelay = " + this.autoCloseDelay);
-				if (this.autoCloseDelay > 0) {
-					this.log("Closing in " + this.autoCloseDelay + " seconds.");
+	getServices() {
+		return [this.informationService, this.service];
+	}
+
+	setupGarageDoorOpenerService(service) {
+		this.log.debug("setupGarageDoorOpenerService");
+		this.log.debug("Cached State: " + this.cachedState);
+
+		if ((this.cachedState === undefined) || (this.cachedState === true)) {
+			this.log.debug("Using Saved OPEN State");
+			this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN);
+		} else {
+			this.log.debug("Using Default CLOSED State");
+			this.service.setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.CLOSED);
+			this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED);
+		}
+
+		service.getCharacteristic(Characteristic.TargetDoorState)
+			.on('get', (callback) => {
+				var targetDoorState = service.getCharacteristic(Characteristic.TargetDoorState).value;
+				callback(null, targetDoorState);
+			})
+			.on('set', (value, callback) => {
+				if (value === Characteristic.TargetDoorState.OPEN) {
+					// Setting door state to OPEN
+					this.lastOpened = new Date();
+
+					this.log(`Opening ${this.name}. Will stay "opening" for ${this.openStateDuration} seconds.`)
+					this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED);
+					this.service.setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.OPEN);
+
 					setTimeout(() => {
-						this.log("Auto Closing");
-						this.service.setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.CLOSED);
+						this.log(`${this.name} set to OPEN. Will stay open for ${this.openStateDuration} seconds.`)
+						this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN);
+						this.storage.setItem(this.name, true);
+
+						setTimeout(() => {
+							this.service.setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.CLOSED);
+						}, this.openStateDuration * 1000);
+					}, this.openingStateDuration * 1000);
+
+					callback();
+
+				} else if (value === Characteristic.TargetDoorState.CLOSED) {
+					this.log(`Closing ${this.name}. Will stay "closing" for ${this.closingStateDuration} seconds.`)
+					this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.OPEN);
+					this.service.setCharacteristic(Characteristic.TargetDoorState, Characteristic.TargetDoorState.CLOSED);
+
+					setTimeout(() => {
+						this.log(`${this.name} set to CLOSED..`)
 						this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED);
 						this.storage.setItem(this.name, false);
-					}, this.autoCloseDelay * 1000);
+						callback();
+					}, this.closingStateDuration * 1000);
+				} else {
+					callback();
 				}
-				
-				callback();
-				
-			} else if (value === Characteristic.TargetDoorState.CLOSED)  {
-				this.log("Closing: " + this.name)
-				this.service.setCharacteristic(Characteristic.CurrentDoorState, Characteristic.CurrentDoorState.CLOSED);
-				this.storage.setItem(this.name, false);
-				callback();
-			} else {
-				callback();
-			}
-		});
+			});
 	}
 }
